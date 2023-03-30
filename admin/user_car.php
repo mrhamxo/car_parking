8ed2f11e7f9b5ec14e09ce1b741a29fe4fe3cdc6
Aamir ullah <105126869+Aamir-Ullah@users.noreply.github.com>
2023-03-30 14:51:23 +0500

    Data inserted in database and fetch all user fro table user_car

--- admin/user_car.php
+++ admin/user_car.php
@@ -1,4 +1,5 @@
 <?php
+include("connectdb.php");
 include('security.php');
 include('includes/header.php');
 include('includes/navbar.php');
@@ -6,87 +7,111 @@ include('includes/navbar.php');
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">User car</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="" method="POST">
-                    <div class="form-group">
-                        <label for="parkingaddress">Parking Address</label>
-                        <input type="text" name="park_address" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Please Enter Parking Address">
-                    </div>
-                    <div class="form-group">
-                        <label>Parking Slots</label>
-                        <select class="form-control" name="park_slots">
-                            <option>Please Add Car Slots</option>
-                            <option value="slots">1</option>
-                            <option value="slots">2</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Parking Area</label>
-                        <input type="password" name="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Please Enter Parking Area">
-                    </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-primary" name="submit">Add</button>
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    </div>
-                </form>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">User car</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="#" method="POST">
+                        <?php
+                        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+                            $par_address = $_POST['park_address'];
+                            $par_slots = $_POST['park_slots'];
+                            $par_area = $_POST['park_area'];
+                            $sql = "INSERT INTO `user_car` (`parking _address`, `parking_slots`, `parking_area`, `status`, `action`) VALUES ('$par_address', '$par_slots', '$par_area', '1', '0')";
+                            $res = mysqli_query($connect, $sql);
+                        }
+                        ?>
+                        <div class="form-group">
+                            <label for="parkingaddress">Parking Address</label>
+                            <input type="text" name="park_address" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Please Enter Parking Address">
+                        </div>
+                        <div class="form-group">
+                            <label>Parking Slots</label>
+                            <select class="form-control" name="park_slots">
+                                <option>Please Add Car Slots</option>
+                                <option value="slots">1</option>
+                                <option value="slots">2</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Parking Area</label>
+                            <input type="text" name="park_area" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Please Enter Parking Area">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary" name="submit">Add</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="container-fluid">
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Add User car
-                <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#exampleModal">
-                    Add User Car
-                </button>
-            </h6>
-        </div>
-        <div class="card-body">
-            <?php
-            // if (isset($_SESSION['success']) && ($_SESSION['success']) != '') {
-            //     echo '<h2 class="my-2 bg-primary text-white">' . $_SESSION['success'] . ' </h2>';
-            //     unset($_SESSION['success']);
-            // }
-            // if (isset($_SESSION['status']) && ($_SESSION['status']) != '') {
-            //     echo '<h2 class="my-2 bg-danger text-white">' . $_SESSION['status'] . ' </h2>';
-            //     unset($_SESSION['status']);
-            // }
-            ?>
+    <div class="container-fluid">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Add User car
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#exampleModal">
+                        Add User Car
+                    </button>
+                </h6>
+            </div>
+            <div class="card-body">
+                <?php
+                // if (isset($_SESSION['success']) && ($_SESSION['success']) != '') {
+                //     echo '<h2 class="my-2 bg-primary text-white">' . $_SESSION['success'] . ' </h2>';
+                //     unset($_SESSION['success']);
+                // }
+                // if (isset($_SESSION['status']) && ($_SESSION['status']) != '') {
+                //     echo '<h2 class="my-2 bg-danger text-white">' . $_SESSION['status'] . ' </h2>';
+                //     unset($_SESSION['status']);
+                // }
+                ?>
 
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Parking Address</th>
-                        <th scope="col">Parking Slots</th>
-                        <th scope="col">Parking Area</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    
-                </tbody>
-            </table>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Parking Address</th>
+                            <th scope="col">Parking Slots</th>
+                            <th scope="col">Parking Area</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql1 = "SELECT * FROM `user_car`";
+                        $res = mysqli_query($connect, $sql1);
+                        while ($row = mysqli_fetch_assoc($res)) {
+                        ?>
+                            <tr>
+                                <td><?php echo $row['id']; ?></td>
+                                <td><?php echo $row['parking _address'];?></td>
+                                <td><?php echo $row['parking_slots']; ?></td>
+                                <td><?php echo $row['parking_area']; ?></td>
+                                <td><?php echo $row['status']; ?></td>
+                                <td><?php echo $row['action']; ?></td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
 
 
-<!-- end container fluid -->
+    <!-- end container fluid -->
 </div>
 </div>
 <?php
